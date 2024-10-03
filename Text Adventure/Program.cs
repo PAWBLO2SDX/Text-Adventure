506e313b32391c1c3473d818b899cca390a397d3
MUKILTEO\1704174 <1704174@mukilteo.wednet.edu>
2024-10-03 12:43:24 -0700

    Added texts and added "if"  statements

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -5,6 +5,15 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.WriteLine("which room would you like to go in");
+
+            string input = Console.ReadLine();
+            Console.WriteLine(input);
+
+            if (input == "VGD" || input == "VGD")
+            {
+                Console.WriteLine("Whatever you choose");
+            }
         }
     }
 }
