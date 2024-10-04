08da18f4ac5d908211217699268ccb3be577c7a9
MUKILTEO\1704174 <1704174@mukilteo.wednet.edu>
2024-10-04 12:24:08 -0700

    The World of Paradox: A Text game filled with damage enimies and health.  the name of the player "Name;" which would consist of (health,damage, and texts with the problem which can occur and a story that comes with each occuring situation)  // Type Casting  double my8byteNumber = 30.45632;  Console.WriteLine(my8byteNumber);

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -1,18 +1,40 @@
-﻿namespace Text_Adventure
+﻿
+
+namespace Text_Adventure
 {
     internal class Program
     {
+      
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.WriteLine("which room would you like to go in");
+            // Type Casting 
+
+            double my8byteNumber = 30.45632;
+            Console.WriteLine(my8byteNumber);
+
+            float my4byteNumber = (float)my8byteNumber;
+            Console.WriteLine(my4byteNumber);
+          
+            int my5byteNumber = (int)my8byteNumber;
+            Console.WriteLine(my5byteNumber);
+
+            // System.Convert
+
+            float my4ByteNumberAgain = Convert.ToSingle(my8byteNumber);
+        
+            Console.WriteLine("Welcome To The World Of Paradox!");
+            Console.WriteLine("Name:");
+            Console.WriteLine("which room would you like to go in?");
+            Console.ReadLine();
 
             string input = Console.ReadLine();
             Console.WriteLine(input);
 
-            if (input == "VGD" || input == "VGD")
+            if (input == "TWP" || input == "TWP")
             {
                 Console.WriteLine("Whatever you choose");
+
+               
             }
         }
     }
