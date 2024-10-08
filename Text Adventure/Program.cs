0014a43e8c7c0e881b25275802312d332a841b25
MUKILTEO\1704174 <1704174@mukilteo.wednet.edu>
2024-10-08 13:39:34 -0700

    I have Added a way for my writing to write by itself and have more things such as the attack and more to the story with damage and health

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -1,57 +1,110 @@
 ﻿using Microsoft.VisualBasic;
+using System;
 
 namespace Text_Adventure
 {
     internal class Program
     {
         public static Player currentPlayer = new Player();
+
+        static void TypeLine(string line, int delay = 50)
+        {
+            for (int i = 0; i < line.Length; i++)
+            {
+                Console.Write(line[i]);
+                Thread.Sleep(delay); // Sleep for 150 milliseconds
+            }
+        }
+
+
         static void Main(string[] args)
 
         {
-            static void TypeLine(string line)
-            {
-                string output = null;
-                char[] characters = line.ToCharArray();
-                for (int i = 0; i < characters.Length; i++)
-                {
-                    output += characters[i];
-                    Console.Clear();
-                }
+            //static void TypeLine(string line, int rInt)
+            //{
+            //    string output = null;
+            //    char[] characters = line.ToCharArray();
+            //    for (int i = 0; i < characters.Length; i++)
+            //    {
+            //        output += characters[i];
+            //        Console.Clear();
+            //    }
 
               
-                Console.WriteLine(output);
-            }
-            Console.WriteLine("The World Of Paradox!");
-           Console.WriteLine("Name:");
-            currentPlayer.name = Console.ReadLine();
+            //    TypeLine(output);
+            //}
+            TypeLine("The World Of Paradox!\r\n", 50);
+            TypeLine("Name: UNKNOWN");
+
             Console.Clear();
-            Console.WriteLine("You awake in the Cold!,Stone, dark room. You feel dazed and are having a trouble time remembering where you are From The Past events");
-             Console.WriteLine("which room would you like to go in?!!!!!");
-            if(currentPlayer.name == "")
-            Console.WriteLine("You can't even remember your own name . . . .");
-           else Console.WriteLine("You Know your name is" + currentPlayer.name);
+            TypeLine("You awake in the Cold!,Stone, dark room. You feel dazed and are having a trouble time remembering where you are From The Past events\r\n");
+             TypeLine("which room would you like to go in?!!!!!\r\n");
+            
+
             Console.ReadKey();
             Console.Clear();
-            Console.WriteLine("You Move around the darkness of the room and have reached to what appears to be a handle of some sort.");
-            Console.WriteLine("YOU MAY TURN IT BUT SOMETHING MIGHT HAPPEN, WHO KNOWSSS??? PRESS (ENTER) TO HOP IN");
+            TypeLine("You and have reached to what appears to be a handle of what it seems to be a doorr .");
+            TypeLine(" TURN IT BUT SOMETHING MIGHT HAPPEN, WHO KNOWSSS??? PRESS (ENTER) TO HOP IN");
             Console.ReadKey();
             Console.Clear();
-            Console.WriteLine("======================================================================================================");
-            Console.WriteLine("|                                      You have Now                                                   |");
-            Console.WriteLine("|                                        Entered                                                      |");
-            Console.WriteLine("|                                    BIG FRIENDLY ROOM                                                |");
-            Console.WriteLine("======================================================================================================");
+            Console.WriteLine("======================================================================================");
+             Console.WriteLine("                               Entered                                               ");
+            Console.WriteLine("                        BIG FRIENDLY ROOM                                            ");
+            Console.WriteLine("====================================================================================");
 
-            Console.WriteLine(" +\r\n                \"You have Encountered a Man who Goes by the name of The Wise One. \");");
-            string input = Console.ReadLine();
-            Console.WriteLine(input);
 
-            if (input == "TWP" || input == "TWP")
+            TypeLine(" +\r\n                \"You have Encountered a Man who Goes by the name of The Wise One. \"");
+
+            TypeLine("As you encounter the man you have options Of Attacking or an option of staying back and being trapped ");
+            
+            Console.ReadKey(true);
+            Console.Clear();
+
+
+            Console.WriteLine("======================================================================================================");
+            Console.WriteLine("                           YES = (E)NTER         OR    NO = I DO NOT KNOW FIGURE IT OUTTTTT        ");
+            Console.WriteLine("=====================================================================================================");
+            Console.ReadKey(true);
+            Console.Clear();
+            Console.WriteLine("==============================================================================================");
+            Console.WriteLine("                   Leo    ");
+            Console.WriteLine("===============================================================================================");
+            Console.WriteLine("|                   (A)ttack  = 10                               (D)efend = 5             ");
+            Console.WriteLine("                          (P)ower = 4                 (H)eals   = 500                 ");
+            Console.WriteLine("==============================================================================================");
+            Console.WriteLine("==============================================================================================");
+            //Encounter Generic 
+             //Encounters        
             {
-                Console.WriteLine("Whatever you choose");
+                TypeLine("You THROW OPEN THE DOOOR!! you grab a  rusty shield That appears infront of you, while charging at the man who captured you");
+                TypeLine(" +\r\n \"He Turns... \"");
+                Console.ReadLine();
+                Console.ReadKey(true);
+                Console.Clear();
+                TypeLine("COMBAT HAS BEEN ENCOUNTERED WITH THE MANNNN");
 
-               
+                Console.WriteLine("===============================================================================================");
+                Console.WriteLine("|            Press (A) to attack         &          (D) Defend    ");
+                Console.WriteLine("==============================================================================================");
+
+                Console.ReadKey(true);
+                Console.Clear();
+                Console.WriteLine("===============================================================================================");
+                Console.WriteLine("|                   (A)ttack  = 10                               (D)efend = 5             ");
+                Console.WriteLine("                          (P)ower = 4                 (H)eals   = 450           ");
+                Console.WriteLine("==============================================================================================");
+                Console.WriteLine("==============================================================================================");
+                Console.WriteLine("The Wise man Has attacked back dealing a total damage of = 50");
+              
+                Console.ReadKey(true);
+                Console.Clear();
+
             }
+
+            
+
+
+          
 
         }
     }
@@ -76,9 +129,7 @@ namespace Text_Adventure
         //Encounters
         public static void FirstEncounter()
         {
-           Console.WriteLine("Your THROW OPEN THE DOOOR!! you grab a  rusty shield That appears infront of you, while charging at the man who captured you");
-            Console.WriteLine("He Turns...");
-            Console.ReadLine();
+           
         }
         
         //Encounter Tools
@@ -101,7 +152,7 @@ namespace Text_Adventure
             {
                 Console.WriteLine("==========================");
                 Console.WriteLine("|(A)ttack       (D)efend |");
-                Console.WriteLine("|     (R)un      (H)eals |");
+                Console.WriteLine("| (P)owereals |");
                 Console.WriteLine("==========================");
                 Console.WriteLine("Shields: " + Program.currentPlayer.shields);
             }
