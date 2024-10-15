e7e5835d182e0ae03ac9782977bd0cae5b884777
MUKILTEO\1704174 <1704174@mukilteo.wednet.edu>
2024-10-15 13:35:03 -0700

    I have added interactions within the game and just completed more of the story while trying to correct my grammer

--- Text Adventure/Program.cs
+++ Text Adventure/Program.cs
@@ -21,124 +21,151 @@ namespace Text_Adventure
 
         static void Main(string[] args)
         {
-            // Display current Foreground color 
-            //Console.WriteLine(Console.ForegroundColor);
 
             // Set the Foreground color to blue 
             Console.ForegroundColor = ConsoleColor.Blue;
 
             // Display current Foreground color 
-            //Console.WriteLine(Console.ForegroundColor);
+           
 
             string Answer = Console.ReadLine();
 
-                              TypeLine("\"Welcome, You have been chosen to Escape The Paradox !\r\n", 90);
-            TypeLine("\"Out of 8 billion people you have been the one to test out our latest invention so welcome to the Room \r\n");
+           TypeLine("\"Welcome, You were "+ " chosen to Escape The Paradox !\r\n", 90);
+
+            Console.WriteLine("\n");
+            TypeLine("\"Out of 8 billion people you have been chosen to be our test subject. latest invention so welcome to the Room \r\n");
 
             TypeLine(" \"We know you are confused, So this is a WARNING!! \r\n");
 
-            TypeLine(" \" The ROOMS you are going to enter 5 rooms in which we are unsure what the causes are for each room since they are undentified  \r\n");
+            TypeLine(" \" They're" +
+                " 5 GATEWAYS given, in which we are unsure what the causes are Behind these Rooms these Rooms has not been identified for any reason");
 
-            TypeLine(" \" You will be Guided through whichever room you have picked  \r\n");
+            TypeLine(" \" You will be Guided through whichever CHAMVBERS  you have Chosen  \r\n");
 
+
+            TypeLine(" \" PRESS  (E)ENTER FOR  NEXT PART.....  \r\n");
 
             Console.ReadKey();
             Console.Clear();
+
+            TypeLine(" \" PART 2  \r\n");
+            Console.WriteLine("\n");
+            TypeLine("");
+            TypeLine(" 10:30 PM \n");
+            Console.WriteLine("\n");
+            TypeLine("You awaken in a stone cold room. your head is spinning and you feel dazed, as if you'd been drugged.\n");
 
             TypeLine("You pick yourself up and infront of you, you blurrily see 5 rooms\n");
-            TypeLine("You awaken in a stone cold room. your head is spinning and you feel dazed, as if you'd been drugged.\n");
-            TypeLine("You do not remember anything, the last thing you remember is the showcase for the project that was built  around 6:30pm. \n");
-            TypeLine("  Name: it cannot be accessed \r\n");
+
+            TypeLine("\"The last thing you remember is the showcase for the project that was built. \n"); 
+            //This is the scene where the Player is in a state of where they can vividly see amd think
+            //The Player encounters Rooms
+            
             Console.ReadKey();
             Console.Clear();
 
-            Console.WriteLine("which room would you like to go in?!!!!!\n");
+            Console.WriteLine("\"which room would you like to go in?\n");
 
-            TypeLine("\n");
+            Console.WriteLine("\n");
 
-            Console.WriteLine("BIG FRIENDLY ROOM \n");
-
-            Console.WriteLine("\"UKNOWN\n");
-
+            Console.WriteLine("\"BIG FRIENDLY ROOM \n");
+           
             Console.WriteLine("\"THE BASEMENT OF FUN\n");
 
-            Console.WriteLine("\"UKNOWN\n");
+            Console.WriteLine("\"Documents \n");
 
             Console.WriteLine("\"UKNOWN\n");
-          
 
+            Console.WriteLine("\"UKNOWN\n");
 
-            TypeLine("You have approached the door,");
+            Console.ReadLine();
+
+                TypeLine("You have approached the door,");
             if (Answer == "BIG FRIENDLY ROOM")
 
             {
-                NiceRoom();
-                static void NiceRoom()
-                {              
-                    Console.ReadKey();
-                    Console.Clear();
-                    Console.WriteLine("======================================================================================");
-                    Console.WriteLine(" =                              Entered                                               =");
-                    Console.WriteLine(" =                        BIG FRIENDLY ROOM                                           = ");
-                    Console.WriteLine("====================================================================================");
-                    Console.ReadKey();
-                    Console.Clear();
-                    Console.ReadKey(true);
-                    Console.Clear();
-                }
+              
             }
-            Console.ReadKey();
-            Console.Clear();
-            TypeLine(" +\r\n                \"                        You have Encountered a Max                     \r\n");
+            
 
-            TypeLine("\n \"You get stunned from the man,he is the one who trapped you the captor is seen with the \n ");
+            BIGFRIENDLYROOM();
+            static void BIGFRIENDLYROOM()
+            {
+                Console.ReadKey();
+                Console.Clear();
+                Console.WriteLine("======================================================================================");
+                Console.WriteLine(" =                              Entered                                               ");
+                Console.WriteLine(" =                        BIG FRIENDLY ROOM                                          ");
+                Console.WriteLine("====================================================================================");
+                Console.ReadKey();
+                Console.Clear();
+            }
+
+            Console.WriteLine("\n");
+            TypeLine("\"You have Encountered Documents, You realize the documents are about who you are as a person. You gained vivid memory \n");
+            TypeLine("\" The memory of who you really are. \n");
 
             Console.ReadKey(true);
             Console.Clear();
             Console.WriteLine("==========================================");
-            Console.WriteLine("                YES = (E)NTER             ");
+            Console.WriteLine("                Documents = TYPE          ");
             Console.WriteLine("===========================================");
-            Console.ReadKey(true);
-            Console.Clear();
-            Console.WriteLine("==============================================================================================");
-            Console.WriteLine("       Name has not been chosen");
-            Console.WriteLine("===============================================================================================");
-            Console.WriteLine("|                   (A)ttack  = 10                               (D)efend = 5             ");
-            Console.WriteLine("                          (P)ower = 4                 (H)eals   = 500                 ");
-            Console.WriteLine("==============================================================================================");
-            Console.WriteLine("==============================================================================================");
+            Console.ReadLine();
+            if (Answer == "Documents")
+
+            {
+
+            }
+
+            Documents();
+            static void Documents()
+            {          
+                Console.WriteLine("==============================================================================================");
+                Console.WriteLine("      0010011 ");
+                Console.WriteLine("===============================================================================================");
+                Console.WriteLine("|                   (A)ttack  = 10                               (D)efend = 5             ");
+                Console.WriteLine("                          (P)ower = 4                 (H)eals   = 500                 ");
+                Console.WriteLine("==============================================================================================");
+                Console.WriteLine("==============================================================================================");
+            }
+       
             //Encounter Generic 
              //Encounters        
             {
-                TypeLine("You open the door gently,   so he wont catch you so you are quiet. ");
-                TypeLine("You are in need of a weapon so you vividly see a shield  nearby you but as you try to grab  it.");
+                TypeLine("\"You open the door gently,\n");
+                TypeLine("\"You are in need of a weapon, so you go and look around the for what you can find around you \n");
+
                 TypeLine("  \"He Turns and looks at you with such anger... \"");
-                TypeLine(" He runs after you when you have the shield and weapons you have contained");
-                TypeLine("");
-                TypeLine("");
-                TypeLine("You open the door gently, in order to be as quiet as a ninja  so he wont catch you. ");
-                TypeLine("You are in need of a weapon so you vividly see a shield but as you go to grab it, while charging at the man who captured you");
+                TypeLine(" \"He runs after you when you have the shield and weapons you have contained\n");                              
+                TypeLine("\"You are in need of a weapon so you vividly see a shield but as you go to grab it, while charging at the man who captured you..... \n");
                 Console.ReadKey(true);
                 Console.Clear();
                 TypeLine("\nCOMBAT HAS BEEN ENCOUNTERED\n");
                 Console.ReadKey(true);
                 Console.Clear();
 
-                Console.WriteLine("===============================================================================================");
-                Console.WriteLine("|              Press (A) to attack          &                  (D) Defend                      ");
-                Console.WriteLine("==============================================================================================");
+                Console.WriteLine("==================================");
+                Console.WriteLine("\"             TYPE to (A) to Attack");
+                Console.WriteLine("================================ ");
+                Console.ReadLine();
+                if (Answer == "A") ;
+                {
 
-                Console.ReadKey(true);
-                Console.Clear();
-                Console.WriteLine("===============================================================================================");
-                Console.WriteLine("|                   (A)ttack  = 10                               (D)efend = 5             ");
-                Console.WriteLine("                          (P)ower = 4                 (H)eals   = 450           ");
-                Console.WriteLine("==============================================================================================");
-                Console.WriteLine("==============================================================================================");
-                Console.WriteLine(" You have done damage to The Wise  your attack has been set ");
-              
-                Console.ReadKey(true);
-                Console.Clear();
+                }
+                Player();
+               static void Player()
+                {
+                    Console.Clear();
+                    Console.WriteLine("===============================================================================================");
+                    Console.WriteLine("|                   (A)ttack  = 10                               (D)efend = 5             ");
+                    Console.WriteLine("                          (P)ower = 4                 (H)eals   = 450           ");
+                    Console.WriteLine("==============================================================================================");
+                    Console.WriteLine("==============================================================================================");
+                    Console.WriteLine(" You have done damage to The Wise  your attack has been set ");
+
+                }
+               
+                
 
             }
 
